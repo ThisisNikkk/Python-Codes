f1c764f8c4cd2838c6019926ac72e147714ecf53
ThisisNikkk <113753784+ThisisNikkk@users.noreply.github.com>
2022-12-30 12:03:23 +0530

    Delete Abstract.py

--- Abstract.py
+++
@@ -1,7 +0,0 @@
-class employee:
-    def data_emp(self):
-        print('Name = Bubu Jangra')
-        print('Department = ThisisNikkk')
-        print('Salary = Papa Kamate Hai')
-obj_data=employee()
-obj_data.data_emp()
